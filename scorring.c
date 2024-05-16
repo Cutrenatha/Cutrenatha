46efdc0f2b026f49ff9212fe962b64d37f0397c3
Cutrenatha <166110072+Cutrenatha@users.noreply.github.com>
2024-05-16 10:50:45 +0700

    Update

--- scorring.c
+++ scorring.c
@@ -11,70 +11,78 @@ int scoring(char * token)
     for(int i=0;i<strlen(token);i++){
         switch (token[i]){
         case 'a':
-        case 'e':
-        case 'm':
-        case 'q':
-            skor+=2;
-            break;
-        case 'b':
-        case 'c':
-        case 'f':
-        case 'g':
-        case 'k':
-        case 'n':
-        case 'o':
-        case 'r':
             skor+=3;
             break;
+        case 'b':
         case 'd':
-        case 'i':
-        case 'l':
-        case 'p':
-        case 'h':
             skor+=4;
             break;
+        case 'c':
+        case 'e':
+        case 'g':
+            skor+=5;
+            break;
+        case 'f':
+        case 'h':
         case 'j':
+            skor+=6;
+            break;
+        case 'i':
+        case 'k':
+        case 'm':
+        case 'p':
+            skor+=7;
+            break;
+        case 'l':
+        case 'n':
+        case 'q':
         case 's':
+            skor+=8;
+            break;
+        case 'o':
+        case 'r':
+        case 't':
+        case 'v':
+            skor+=9;
+            break;
         case 'u':
         case 'w':
         case 'y':
-            skor+=5;
-            break;
-        case 't':
-        case 'v':
-        case 'x':
-        case 'z':
-            skor+=6;
-            break;
-        case '1':
-            skor+=7;
-            break;
-        case '2':
-            skor+=8;
-            break;
-        case '3':
-            skor+=9;
-            break;
-        case '4':
             skor+=10;
             break;
-        case '5':
+        case 'x':
+        case 'z':
             skor+=11;
             break;
-        case '6':
+        case '1':
             skor+=12;
             break;
-        case '7':
+        case '2':
             skor+=13;
             break;
-        case '8':
+        case '3':
             skor+=14;
             break;
-        case '9':
+        case '4':
             skor+=15;
             break;
-        case '0':
+        case '5':
             skor+=16;
+            break;
+        case '6':
+            skor+=17;
+            break;
+        case '7':
+            skor+=18;
+            break;
+        case '8':
+            skor+=19;
+            break;
+        case '9':
+            skor+=20;
+            break;
+        case '0':
+            skor+=21;
             break;
         }
     }
